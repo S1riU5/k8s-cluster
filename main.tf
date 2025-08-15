64bde9d9f32331447f5f4c2804b67339367c47d1
Simon Gyimah (ext.) <simon.gyimah@partner.bmw.de>
2025-08-15 16:11:24 +0200

    Add minimal kubernetes cluster setup

---
+++ main.tf
@@ -0,0 +1,12 @@
+module "kubernetes" {
+  source  = "hcloud-k8s/kubernetes/hcloud"
+  version = "2.7.0"
+  cluster_name = "barra_playground"
+  control_plane_nodepools = [
+    { name = "control", type = "cpx21", location = "fsn1", count = 3 }
+  ]
+  worker_nodepools = [
+    { name = "worker", type = "cpx11", location = "fsn1", count = 3 }
+  ]
+  ]
+}
